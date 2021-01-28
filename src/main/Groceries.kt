0fc6f2ce510fdbe9c3b38e230a355d3d0a535925
Chris Ottersen <christopherottersen@gmail.com>
2021-01-28 15:22:38 -0800

    Step 4 Limited Stock NOTE: I am just hardcoding in a starting stock of 20 apples and 20 oranges.

--- src/main/Groceries.kt
+++ src/main/Groceries.kt
@@ -1,5 +1,7 @@
 const val APPLE_PRICE: Double = 0.60
 const val ORANGE_PRICE: Double = 0.25
+var appleStock: Int = 20
+var orangeStock: Int = 20
 fun main() {
     println("Please enter order")
     val orderString: String = readLine() as String
@@ -8,9 +10,9 @@ fun main() {
 
 /**
  * @param orderString a string comprised of 0 or more instances of "apple" and "orange" separated by "," and/or " "
- * @return a formatted string reporting the order and price
+ * @return the total price
  */
-fun calculatePrice(orderString: String):Double {
+fun calculatePrice(orderString: String): Double {
 
     var apples: Int = 0
     var oranges: Int = 0
@@ -21,6 +23,16 @@ fun calculatePrice(orderString: String):Double {
             oranges++
         }
     }
+    when {
+        appleStock < apples && orangeStock < oranges ->
+            throw Exception("only %d apples and %d oranges in stock".format(apples, oranges))
+        appleStock < apples ->
+            throw Exception("only %d apples in stock".format(apples))
+        orangeStock < oranges ->
+            throw Exception("only %d oranges in stock".format(oranges))
+    }
+    appleStock -= apples
+    orangeStock -= oranges
     val total: Double = (apples / 2 + apples % 2) * APPLE_PRICE + (oranges / 3 * 2 + oranges % 3) * ORANGE_PRICE
     println("the cost of %2d apples and %2d oranges is: $%.2f".format(oranges, apples, total))
     return total
